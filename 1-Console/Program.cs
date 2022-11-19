19203908a831e9ec85805f1cebb4babbba434ea8
bowringj <jay@datamaster.co.uk>
2022-11-19 17:33:03 +0000

    Add project files.

---
+++ 1-Console/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _1_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            int? age = null;
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Hello {name}, your age is {age}");
+
+            Console.WriteLine("\nPress any key to continue ...");
+            Console.ReadKey();
+        }
+    }
+}
