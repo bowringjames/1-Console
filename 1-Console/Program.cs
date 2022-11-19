939744ca4894b74285b14c3a994436df6ce83101
bowringj <jay@datamaster.co.uk>
2022-11-19 17:38:57 +0000

    Updated, as I though converting string input to int without, at least, a TryParse was bad practice and students might as well learn the correct way from the start

--- 1-Console/Program.cs
+++ 1-Console/Program.cs
@@ -5,13 +5,13 @@
         static void Main(string[] args)
         {
             string name = "";
-            int? age = null;
+            int age;
 
             Console.WriteLine("What is your name?");
             name = Console.ReadLine();
 
             Console.WriteLine("What is your age?");
-            age = Convert.ToInt32(Console.ReadLine());
+            int.TryParse(Console.ReadLine(), out age);
 
             Console.WriteLine($"Hello {name}, your age is {age}");
 
